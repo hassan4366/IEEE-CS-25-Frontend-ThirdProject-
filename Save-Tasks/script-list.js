ec57d06f3e470fda04d79a08698d29dde1e26777
Hassan Hammam <hassanibrahimhammam@gmail.com>
2025-05-07 00:20:10 +0300

    New Commit

--- Save-Tasks/script-list.js
+++ Save-Tasks/script-list.js
@@ -4,3 +4,20 @@ let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 function saveTasks() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  if (!text) return alert("Please enter a task");
+
+  const now = new Date();
+  const task = {
+    text,
+    status: "none",
+    time: `${now.toLocaleDateString()} - ${now.getHours()}:${now.getMinutes()}`,
+  };
+
+  tasks.push(task);
+  saveTasks();
+  displayTasks();
+  taskInput.value = "";
+}
